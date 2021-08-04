01f4fcbaf06c7ac3afe95ed4d1a430e386cbd0db
Nico Hebein <nico.hebein@dynatrace.com>
2021-08-04 10:52:00 +0200

    add neccessary field and annotation for parsing
    Signed-off-by: Nico Hebein <nico.hebein@dynatrace.com>

--- src/main/java/com/dynatrace/prototype/domainModel/eventData/KeptnCloudEventGetSLIData.java
+++ src/main/java/com/dynatrace/prototype/domainModel/eventData/KeptnCloudEventGetSLIData.java
@@ -1,11 +1,15 @@
 package com.dynatrace.prototype.domainModel.eventData;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.LinkedHashMap;
 
 public class KeptnCloudEventGetSLIData extends KeptnCloudEventData {
     private static final String SLI_PROVIDER = "sliProvider";
+    @JsonProperty(value = "get-sli")
     private LinkedHashMap<String, ?> getSli;
     private LinkedHashMap<String, ?> customFilters;
+    private String deployment;
 
     public LinkedHashMap<String, ?> getGetSli() {
         return getSli;
@@ -13,6 +17,10 @@ public class KeptnCloudEventGetSLIData extends KeptnCloudEventData {
 
     public LinkedHashMap<String, ?> getCustomFilters() {
         return customFilters;
+    }
+
+    public String getDeployment() {
+        return deployment;
     }
 
     public String getSliProvider() {
