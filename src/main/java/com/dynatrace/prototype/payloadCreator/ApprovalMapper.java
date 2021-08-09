9c541ceec1712f861e37b7c5fc8a83d1cc9e5142
Nico Hebein <nico.hebein@dynatrace.com>
2021-08-09 09:58:38 +0200

    change the message of the approval event
    Signed-off-by: Nico Hebein <nico.hebein@dynatrace.com>

--- src/main/java/com/dynatrace/prototype/payloadCreator/ApprovalMapper.java
+++ src/main/java/com/dynatrace/prototype/payloadCreator/ApprovalMapper.java
@@ -4,8 +4,11 @@ import com.dynatrace.prototype.domainModel.KeptnCloudEvent;
 import com.dynatrace.prototype.domainModel.KeptnCloudEventDataResult;
 import com.dynatrace.prototype.domainModel.KeptnEvent;
 import com.dynatrace.prototype.domainModel.eventData.KeptnCloudEventApprovalData;
+<<<<<<< HEAD
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+=======
+>>>>>>> aa0befd (change the message of the approval event)
 import com.slack.api.model.block.ActionsBlock;
 import com.slack.api.model.block.LayoutBlock;
 import com.slack.api.model.block.SectionBlock;
@@ -23,10 +26,15 @@ public class ApprovalMapper extends KeptnCloudEventMapper {
     private static final String CONFIRM_TEXT = "Are you sure that you want to %s it?";
     private static final String CONFIRM_YES = "yes";
     private static final String CONFIRM_NO = "cancel";
+<<<<<<< HEAD
     public static final String APPROVAL_APPROVE_VALUE = "approve";
     public static final String APPROVAL_DENY_VALUE = "deny";
     public static final String APPROVAL_APPROVE_ID = "approve";
     public static final String APPROVAL_DENY_ID = "deny";
+=======
+    private static final String APPROVAL_APPROVE = "approve";
+    private static final String APPROVAL_DENY = "deny";
+>>>>>>> aa0befd (change the message of the approval event)
 
     @Override
     public List<LayoutBlock> getSpecificData(KeptnCloudEvent event) {
@@ -61,6 +69,7 @@ public class ApprovalMapper extends KeptnCloudEventMapper {
             } else if (project == null) {
                 System.err.println("Project of approval is null!");
             } else {
+<<<<<<< HEAD
                 if (KeptnCloudEventDataResult.PASS.equals(result) && MANUAL.equals(pass) ||
                         KeptnCloudEventDataResult.WARNING.equals(result) && MANUAL.equals(warning)) {
                     message.append(String.format("Do you want to promote from stage '%1$s' the service '%2$s' of the project '%3$s'?", stage, service, project));
@@ -68,6 +77,14 @@ public class ApprovalMapper extends KeptnCloudEventMapper {
                 } else if (KeptnCloudEventDataResult.FAIL.equals(result)) {
                     message.append(String.format("There was an error when approving the service '%1$s' from stage '%2$s' of the project '%3$s'.", service, stage, project));
                 } else {
+=======
+                if (KeptnCloudEventDataResult.PASS.getValue().equals(result.getValue()) && MANUAL.equals(pass) ||
+                        KeptnCloudEventDataResult.WARNING.getValue().equals(result.getValue()) && MANUAL.equals(warning)) {
+                    message.append(String.format("Do you want to promote from stage '%1$s' the service '%2$s' of the project '%3$s'?", stage, service, project));
+                    manual = true;
+                } else {
+                    //TODO: what should I do if the result is "fail"?
+>>>>>>> aa0befd (change the message of the approval event)
                     message.append(String.format("The service '%1$s' from stage '%2$s' of the project '%3$s' was promoted.", service, stage, project));
                 }
             }
@@ -76,6 +93,7 @@ public class ApprovalMapper extends KeptnCloudEventMapper {
                 layoutBlockList.add(createSlackBlock(SectionBlock.TYPE, message.toString()));
                 if (manual) {
                     List<BlockElement> buttons = new ArrayList<>();
+<<<<<<< HEAD
                     ConfirmationDialogObject confirmationApprove = createSlackConfirmationDialog(CONFIRM_TITLE, String.format(CONFIRM_TEXT, APPROVAL_APPROVE_VALUE), CONFIRM_YES, CONFIRM_NO, SLACK_STYLE_PRIMARY);
                     ConfirmationDialogObject confirmationDeny = createSlackConfirmationDialog(CONFIRM_TITLE, String.format(CONFIRM_TEXT, APPROVAL_DENY_VALUE), CONFIRM_YES, CONFIRM_NO, SLACK_STYLE_DANGER);
 
@@ -88,6 +106,13 @@ public class ApprovalMapper extends KeptnCloudEventMapper {
                     } catch (JsonProcessingException e) {
                         e.printStackTrace();
                     }
+=======
+                    ConfirmationDialogObject confirmationApprove = createSlackConfirmationDialog(CONFIRM_TITLE, String.format(CONFIRM_TEXT, APPROVAL_APPROVE), CONFIRM_YES, CONFIRM_NO, SLACK_STYLE_PRIMARY);
+                    ConfirmationDialogObject confirmationDeny = createSlackConfirmationDialog(CONFIRM_TITLE, String.format(CONFIRM_TEXT, APPROVAL_DENY), CONFIRM_YES, CONFIRM_NO, SLACK_STYLE_DANGER);
+
+                    buttons.add(createSlackButton(APPROVAL_APPROVE, "yes", SLACK_STYLE_PRIMARY, confirmationApprove));
+                    buttons.add(createSlackButton(APPROVAL_DENY, "no", SLACK_STYLE_DANGER, confirmationDeny));
+>>>>>>> aa0befd (change the message of the approval event)
 
                     layoutBlockList.add(createSlackBlock(ActionsBlock.TYPE, buttons));
                 }
