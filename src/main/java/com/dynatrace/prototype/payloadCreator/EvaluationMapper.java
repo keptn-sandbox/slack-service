bc8b2b3844e764b196e16a3445a99665c34af5e9
Nico Hebein <nico.hebein@dynatrace.com>
2021-08-12 11:26:02 +0200

    change the evaluation msg not finished though
    Signed-off-by: Nico Hebein <nico.hebein@dynatrace.com>

--- src/main/java/com/dynatrace/prototype/payloadCreator/EvaluationMapper.java
+++ src/main/java/com/dynatrace/prototype/payloadCreator/EvaluationMapper.java
@@ -45,15 +45,14 @@ public class EvaluationMapper extends KeptnCloudEventMapper {
 
                 if (sliResIterator.hasNext()) {
                     specificDataSB.append("SLI:\n");
-                    specificDataSB.append(String.format("%-20s|", "Name")).append(String.format("%-20s|", "Value"))
-                            .append(String.format("%-20s|", "pass Criteria")).append(String.format("%-20s|", "warning Criteria"))
-                            .append(String.format("%-20s|", "Result")).append("Score\n");
+                    specificDataSB.append("Name").append(" | Value").append(" | pass Criteria").append(" | warning Criteria")
+                            .append(" | Result").append(" | Score\n");
                     while (sliResIterator.hasNext()) {
                         SLIEvaluationResult element = sliResIterator.next();
-                        specificDataSB.append(element.getDisplayName()).append("\t|\t").append(element.getValue().getValue())
-                                .append("\t|\t").append(element.getPassTargets().toString()).append("\t|\t")
-                                .append(element.getWarningTargets().toString()).append("\t|\t").append(element.getStatus())
-                                .append("\t|\t").append(element.getScore()).append("\n");
+                        specificDataSB.append(element.getDisplayName()).append(" | ").append(element.getValue().getValue())
+                                .append(" | ").append(element.getPassTargets().toString()).append(" | ")
+                                .append(element.getWarningTargets().toString()).append(" | ").append(element.getStatus())
+                                .append(" | ").append(element.getScore()).append("\n");
                     }
                 }
             }
